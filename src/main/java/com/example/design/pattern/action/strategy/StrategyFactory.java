1bc6408cc36d0aec70efffdc6bc028f44069f1cd
Yeats <yeatsinheart@gmail.com>
2019-05-17 20:56:15 +0800

    初始化项目

--- src/main/java/com/example/design/pattern/action/strategy/StrategyFactory.java
+++ src/main/java/com/example/design/pattern/action/strategy/StrategyFactory.java
@@ -26,14 +26,11 @@ public class StrategyFactory {
     private Map<String, StrategyService> targetStrategyService;
 
     public StrategyService getStrategyServiceByServiceName(String tag) {
-        StrategyService strategyService = strategyServiceMap.getOrDefault(tag, defaultStrategy);
-        if (null != strategyService) {
-            targetStrategyService.put(tag, strategyService);
-        }
-        return strategyService;
+        return strategyServiceMap.getOrDefault(tag, defaultStrategy);
     }
 
     //自定义tag。。可以考虑选定一些生成规则来区分  业务线--策略几
+    //String name =strategyFactory.getClass().getSimpleName(); 也可以根据类名直接调用相关识别码，比如类型转换
     public StrategyService getStrategyServiceByBusiness(String tag) {
         StrategyService strategyService = targetStrategyService.getOrDefault(tag, strategyServiceList.stream().filter(f -> {
             Strategy[] annotations = f.getClass().getAnnotationsByType(Strategy.class);
